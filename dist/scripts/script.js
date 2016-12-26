03ce750dad98367e99859a1b288f5d5ea890393d
ArturGodunov <artur-godunov@mail.ru>
2016-12-26 20:03:36 +0300

    add slider

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -9,11 +9,17 @@ var app = (function () {
         DETAILS_CARD_BUTTON_TITLE = 'Купить сейчас',
         SRC_IMG = 'images/',
         ID_DETAILS_CARD = 'detailsCard',
+        ID_DETAILS_CARD_IMG = 'detailsCardImg',
         ID_DETAILS_CARD_TITLE = 'detailsCardTitle',
         ID_DETAILS_CARD_DESC = 'detailsCardDesc',
         CLASS_NAME_DETAILS_CARD_CONTENT = 'u-align--container u-box-shadow u-background--white',
         CLASS_NAME_DETAILS_CARD_TITLE = 'u-margin-title--m u-font-size--3xl',
-        DATA_ATTR_GOODS_LINK = 'data-goods-link';
+        CLASS_NAME_NAV_SLIDER = 'c-nav-slider',
+        CLASS_NAME_NAV_SLIDER_ITEM = 'c-nav-slider-item',
+        CLASS_NAME_NAV_IMG_SLIDER = 'c-nav-img-slider',
+        CLASS_NAME_NAV_IMG_SLIDER_ITEM = 'c-nav-img-slider-item u-box-shadow u-bg-images--contain',
+        DATA_ATTR_GOODS_LINK = 'data-goods-link',
+        DATA_ATTR_SLIDE = 'data-slide';
 
     var store;
 
@@ -41,7 +47,11 @@ var app = (function () {
 
                     return;
                 } else {
-                    target = target.parentNode;
+                    if (target.tagName !== 'BODY') {
+                        target = target.parentNode;
+                    } else {
+                        return;
+                    }
                 }
             }
         });
@@ -96,7 +106,7 @@ var app = (function () {
             detailsCardTitle = document.createElement('h2'),
             detailsCardContent = document.createElement('section'),
             itemContent =
-                '<div class="c-details-card-img u-bg-images--contain"' +
+                '<div class="c-details-card-img u-bg-images--contain" id=' + ID_DETAILS_CARD_IMG +
                 ' style=\'background-image: url("' + SRC_IMG + detailItem.img2 + '");\'></div>' +
                 '<div class="c-details-card-desc">' +
                 '<p class="u-margin-title--sm u-font-size--xxl">' + detailItem.costEU + '</p>' +
@@ -119,6 +129,84 @@ var app = (function () {
 
             detailsCard.appendChild(detailsCardTitle);
             detailsCard.appendChild(detailsCardContent);
+        }
+
+        buildDetailsCardImgSlider(detailItem);
+    };
+
+    /**
+     * Build slider of images in details card
+     * */
+    var buildDetailsCardImgSlider = function (detailItem) {
+        var detailsImgArr = detailItem.img3 ? detailItem.img3 : '';
+
+        if (detailsImgArr.length) {
+            var navSlider = document.createElement('ul'),
+                navImgSlider = document.createElement('ul');
+
+            navSlider.className = CLASS_NAME_NAV_SLIDER;
+            navImgSlider.className = CLASS_NAME_NAV_IMG_SLIDER;
+
+            detailsImgArr.forEach(function (item, i) {
+                var navSliderItem = document.createElement('li'),
+                    navImgSliderItem = document.createElement('li');
+
+                navSliderItem.className = i === 0 ? CLASS_NAME_NAV_SLIDER_ITEM + ' active' : CLASS_NAME_NAV_SLIDER_ITEM;
+                navSliderItem.setAttribute(DATA_ATTR_SLIDE, i);
+
+                navImgSliderItem.className =
+                    i === 0 ? CLASS_NAME_NAV_IMG_SLIDER_ITEM + ' active' : CLASS_NAME_NAV_IMG_SLIDER_ITEM;
+                navImgSliderItem.setAttribute('style', 'background-image: url("' + SRC_IMG + item + '")');
+                navImgSliderItem.setAttribute(DATA_ATTR_SLIDE, i);
+
+                navSlider.appendChild(navSliderItem);
+                navImgSlider.appendChild(navImgSliderItem);
+            });
+
+            document.getElementById(ID_DETAILS_CARD_IMG).appendChild(navSlider);
+            document.getElementById(ID_DETAILS_CARD).appendChild(navImgSlider);
+
+            onChangeSliderItem(navSlider, navImgSlider, detailItem);
+        }
+    };
+
+    /**
+     * Change slides in details card slider
+     * */
+    var onChangeSliderItem = function (navSlider, navImgSlider, detailItem) {
+        navSlider.addEventListener('click', onChangeSlide);
+        navImgSlider.addEventListener('click', onChangeSlide);
+
+        function onChangeSlide() {
+            var target = event.target;
+
+            while (!target.hasAttribute(DATA_ATTR_SLIDE) || target !== navSlider || target !== navImgSlider) {
+                if (target.hasAttribute(DATA_ATTR_SLIDE)) {
+                    var slideNumber = +target.getAttribute(DATA_ATTR_SLIDE);
+
+                    document.getElementById(ID_DETAILS_CARD_IMG).style.backgroundImage =
+                        'url("' + SRC_IMG + detailItem.img3[slideNumber] + '")';
+
+                    var navSliderAll = document.querySelectorAll('[' + DATA_ATTR_SLIDE + ']');
+                    Array.prototype.forEach.call(navSliderAll, function (item) {
+                        var itemNumber = +item.getAttribute(DATA_ATTR_SLIDE);
+
+                        if (itemNumber !== slideNumber) {
+                            item.classList.remove('active');
+                        } else {
+                            item.classList.add('active');
+                        }
+                    });
+
+                    return;
+                } else {
+                    if (target.tagName !== 'BODY') {
+                        target = target.parentNode;
+                    } else {
+                        return;
+                    }
+                }
+            }
         }
     };
 
