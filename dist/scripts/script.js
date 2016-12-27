ac80cdc90798e2b1eb346ccd5f962559e5ab31b4
ArturGodunov <artur-godunov@mail.ru>
2016-12-27 22:09:01 +0300

    less-js first commit

--- dist/scripts/script.js
+++ dist/scripts/script.js
@@ -5,22 +5,7 @@ var app = (function () {
      * Constants
      * */
     var URL_DATA = 'goods.json',
-        GOODS_HOVER_BUTTON_TITLE = 'Посмотреть и купить',
-        DETAILS_CARD_BUTTON_TITLE = 'Купить сейчас',
-        SRC_IMG = 'images/',
-        ID_DETAILS_CARD = 'detailsCard',
-        ID_DETAILS_CARD_IMG = 'detailsCardImg',
-        ID_DETAILS_CARD_TITLE = 'detailsCardTitle',
-        ID_DETAILS_CARD_DESC = 'detailsCardDesc',
-        ID_NAV_IMG_SLIDER = 'detailsCardNavImgSlider',
-        CLASS_NAME_DETAILS_CARD_CONTENT = 'u-align--container u-box-shadow u-background--white',
-        CLASS_NAME_NAV_SLIDER = 'c-nav-slider',
-        CLASS_NAME_NAV_SLIDER_ITEM = 'c-nav-slider-item',
-        CLASS_NAME_NAV_IMG_SLIDER = 'c-nav-img-slider',
-        CLASS_NAME_NAV_IMG_SLIDER_ITEM = 'c-nav-img-slider-item u-box-shadow u-bg-images--contain',
-        CLASS_NAME_DETAILS_CARD_HEADER = 'c-details-card-header',
-        DATA_ATTR_GOODS_LINK = 'data-goods-link',
-        DATA_ATTR_SLIDE = 'data-slide';
+        SRC_IMG = 'images/';
 
     var store;
 
@@ -34,16 +19,103 @@ var app = (function () {
     };
 
     /**
+     *
+     * */
+    var buildGoods = function (data) {
+        var goods = document.getElementById('goods');
+
+        data.forEach(function (item) {
+            var goodsItemResourceInnerHTML = document.getElementById('goodsItemResource').innerHTML,
+                newGoodsItem = document.createElement('li');
+
+            newGoodsItem.className = 'c-goods-item u-box-shadow';
+
+            goodsItemResourceInnerHTML = goodsItemResourceInnerHTML.replace('%goodsItemImg%', SRC_IMG + item.img);
+            goodsItemResourceInnerHTML = goodsItemResourceInnerHTML.replace('%goodsItemLinkReplace%', item.id);
+            goodsItemResourceInnerHTML = goodsItemResourceInnerHTML.replace('%goodsItemNameReplace%', item.name);
+            goodsItemResourceInnerHTML = goodsItemResourceInnerHTML.replace('%goodsItemCostReplace%', item.costEU);
+
+            newGoodsItem.innerHTML = goodsItemResourceInnerHTML;
+
+            goods.appendChild(newGoodsItem);
+        });
+    };
+
+    /**
+     *
+     * */
+    var buildDetailsCard = function (hash) {
+        var detailsCardOld = document.getElementById('detailsCard');
+        if (detailsCardOld) {
+            detailsCardOld.parentNode.removeChild(detailsCardOld);
+        }
+
+        var id = hash.replace('#', ''),
+            detailItem = store.filter(function (item) {
+                return item.id === id;
+            })[0];
+
+        var detailsCardResource = document.getElementById('detailsCardResource'),
+            detailsCardResourceInnerHTML = detailsCardResource.innerHTML,
+            newDetailsCard = document.createElement('section');
+
+        newDetailsCard.className = 'c-details-card';
+        newDetailsCard.id = 'detailsCard';
+
+        detailsCardResourceInnerHTML = detailsCardResourceInnerHTML.replace('%detailsCardTitleReplace%', detailItem.name);
+        detailsCardResourceInnerHTML = detailsCardResourceInnerHTML.replace('%detailsCardImgReplace%', SRC_IMG + detailItem.img2);
+        detailsCardResourceInnerHTML = detailsCardResourceInnerHTML.replace('%detailsCardDescDataSlideReplace%', detailItem.id);
+        detailsCardResourceInnerHTML = detailsCardResourceInnerHTML.replace('%detailsCardDescCostEUReplace%', detailItem.costEU);
+        detailsCardResourceInnerHTML = detailsCardResourceInnerHTML.replace('%detailsCardDescCostBRReplace%', detailItem.costBR);
+        detailsCardResourceInnerHTML = detailsCardResourceInnerHTML.replace('%detailsCardDescDescReplace%', detailItem.desc);
+
+        newDetailsCard.innerHTML = detailsCardResourceInnerHTML;
+
+        detailsCardResource.parentNode.insertBefore(newDetailsCard, detailsCardResource);
+
+        buildDetailsCardImgSlider(detailItem);
+    };
+
+    /**
+     *
+     * */
+    var buildDetailsCardImgSlider = function (detailItem) {
+        var detailsImgArr = detailItem.img3 ? detailItem.img3 : '';
+
+        if (detailsImgArr.length) {
+            var navSliderResource = document.getElementById('navSliderResource'),
+                navImgSliderResource = document.getElementById('navImgSliderResource');
+
+            detailsImgArr.forEach(function (item, i) {
+                var navSliderItem =  document.createElement('li');
+
+                navSliderItem.className = 'c-nav-slider-item';
+                navSliderItem.setAttribute('data-slide', i);
+
+                navSliderResource.appendChild(navSliderItem);
+
+                var navImgSliderItem =  document.createElement('li');
+
+                navImgSliderItem.className = 'c-nav-img-slider-item u-box-shadow u-bg-images--contain';
+                navImgSliderItem.style.backgroundImage = 'url("' + SRC_IMG + item + '")';
+
+                navImgSliderResource.appendChild(navImgSliderItem);
+            });
+        }
+    };
+
+    /**
      * Click on goods item
      * */
     var onChooseGoods = function () {
         var goods = document.getElementById('goods');
+
         goods.addEventListener('click', function (event) {
             event.preventDefault();
             var target = event.target;
 
-            while (!target.hasAttribute(DATA_ATTR_GOODS_LINK) || target !== goods) {
-                if (target.hasAttribute(DATA_ATTR_GOODS_LINK)) {
+            while (!target.hasAttribute('data-goods-link') || target !== goods) {
+                if (target.hasAttribute('data-goods-link')) {
                     window.location.hash = target.getAttribute('href');
 
                     return;
@@ -67,165 +139,6 @@ var app = (function () {
 
             buildDetailsCard(hash);
         });
-    };
-
-    /**
-     * Build goods
-     * */
-    var buildGoods = function (data) {
-        var goods = document.getElementById('goods');
-
-        data.forEach(function (item) {
-            var li = document.createElement('li');
-            li.className = 'c-goods-item u-box-shadow';
-
-            li.innerHTML =
-                '<a href="' + item.id + '" ' + DATA_ATTR_GOODS_LINK + ' class="c-goods-link u-bg-images--contain"' +
-                ' style=\'background-image: url("' + SRC_IMG + item.img + '");\'>' +
-                '<span class="c-goods-link-hover">' +
-                '<span class="c-goods-link-hover-text">' + GOODS_HOVER_BUTTON_TITLE + '</span>' +
-                '</span>' +
-                '</a>' +
-                '<p class="c-goods-sign">' + item.name +
-                '<strong class="u-display--block u-font-weight--bold">' + item.costEU + '</strong>' +
-                '</p>';
-
-            goods.appendChild(li);
-        });
-    };
-
-    /**
-     * Build details card
-     * */
-    var buildDetailsCard = function (hash) {
-        var id = hash.replace('#', ''),
-            detailItem = store.filter(function (item) {
-                return item.id === id;
-            })[0];
-
-        var detailsCard = document.getElementById(ID_DETAILS_CARD),
-            detailsCardHeader = document.createElement('header'),
-            detailsCardTitle = document.createElement('h2'),
-            detailsCardContent = document.createElement('section'),
-            itemContent =
-                '<div class="c-details-card-img u-bg-images--contain" id=' + ID_DETAILS_CARD_IMG +
-                ' style=\'background-image: url("' + SRC_IMG + detailItem.img2 + '");\'>' +
-                '</div>' +
-                '<div class="c-details-card-desc">' +
-                '<p class="u-margin-title--sm u-font-size--xxl">' + detailItem.costEU + '</p>' +
-                '<p>' + detailItem.costBR + '</p>' +
-                '<a href="" class="c-btn c-btn--m c-btn--block c-btn--green u-font-size--l">' + DETAILS_CARD_BUTTON_TITLE + '</a>' +
-                '<p class="u-font-color--grey-lighter u-line-height--big">' + detailItem.desc + '</p>' +
-                '<div class="u-margin--top-auto u-padding-top u-align--container-between">' +
-                '<a href="" class="c-btn c-btn--sm c-btn--border-grey u-margin--top-sm">Возврат и обмен</a>' +
-                '<a href="" class="c-btn c-btn--sm c-btn--border-grey u-margin--top-sm">Пожаловаться</a>' +
-                '</div>' +
-                '</div>';
-
-        if (detailsCard.children.length) {
-            document.getElementById(ID_DETAILS_CARD_TITLE).innerHTML = detailItem.name;
-            document.getElementById(ID_DETAILS_CARD_DESC).innerHTML = itemContent;
-        } else {
-            detailsCardHeader.className = CLASS_NAME_DETAILS_CARD_HEADER;
-            detailsCardHeader.innerHTML =
-                '<h2 class="u-margin-title--m u-font-size--3xl u-align--center" id="detailsCardTitle">' + detailItem.name + '</h2>' +
-                '<div class="u-align--container-between">' +
-                '<a href="" class="c-btn c-btn--m c-btn--border-green c-icon c-icon-share u-margin--right-sm"></a>' +
-                '<a href="" class="c-btn c-btn--m c-btn--border-green c-icon c-icon-chat">Написать продавцу в чат</a>' +
-                '</div>';
-
-            detailsCardContent.className = CLASS_NAME_DETAILS_CARD_CONTENT;
-            detailsCardContent.id = ID_DETAILS_CARD_DESC;
-            detailsCardContent.innerHTML = itemContent;
-
-            detailsCard.appendChild(detailsCardHeader);
-            detailsCard.appendChild(detailsCardContent);
-        }
-
-        buildDetailsCardImgSlider(detailItem);
-    };
-
-    /**
-     * Build slider of images in details card
-     * */
-    var buildDetailsCardImgSlider = function (detailItem) {
-        var detailsImgArr = detailItem.img3 ? detailItem.img3 : '',
-            navImgSliderPrevious = document.getElementById(ID_NAV_IMG_SLIDER),
-            detailsCard = document.getElementById(ID_DETAILS_CARD);
-
-        if (navImgSliderPrevious) {
-            detailsCard.removeChild(navImgSliderPrevious);
-        }
-
-        if (detailsImgArr.length) {
-            var navSlider = document.createElement('ul'),
-                navImgSlider = document.createElement('ul');
-
-            navSlider.className = CLASS_NAME_NAV_SLIDER;
-            navImgSlider.className = CLASS_NAME_NAV_IMG_SLIDER;
-            navImgSlider.id = ID_NAV_IMG_SLIDER;
-
-            detailsImgArr.forEach(function (item, i) {
-                var navSliderItem = document.createElement('li'),
-                    navImgSliderItem = document.createElement('li');
-
-                navSliderItem.className = i === 0 ? CLASS_NAME_NAV_SLIDER_ITEM + ' active' : CLASS_NAME_NAV_SLIDER_ITEM;
-                navSliderItem.setAttribute(DATA_ATTR_SLIDE, i);
-
-                navImgSliderItem.className =
-                    i === 0 ? CLASS_NAME_NAV_IMG_SLIDER_ITEM + ' active' : CLASS_NAME_NAV_IMG_SLIDER_ITEM;
-                navImgSliderItem.setAttribute('style', 'background-image: url("' + SRC_IMG + item + '")');
-                navImgSliderItem.setAttribute(DATA_ATTR_SLIDE, i);
-
-                navSlider.appendChild(navSliderItem);
-                navImgSlider.appendChild(navImgSliderItem);
-            });
-
-            document.getElementById(ID_DETAILS_CARD_IMG).appendChild(navSlider);
-            document.getElementById(ID_DETAILS_CARD).appendChild(navImgSlider);
-
-            onChangeSliderItem(navSlider, navImgSlider, detailItem);
-        }
-    };
-
-    /**
-     * Change slides in details card slider
-     * */
-    var onChangeSliderItem = function (navSlider, navImgSlider, detailItem) {
-        navSlider.addEventListener('click', onChangeSlide);
-        navImgSlider.addEventListener('click', onChangeSlide);
-
-        function onChangeSlide() {
-            var target = event.target;
-
-            while (!target.hasAttribute(DATA_ATTR_SLIDE) || target !== navSlider || target !== navImgSlider) {
-                if (target.hasAttribute(DATA_ATTR_SLIDE)) {
-                    var slideNumber = +target.getAttribute(DATA_ATTR_SLIDE);
-
-                    document.getElementById(ID_DETAILS_CARD_IMG).style.backgroundImage =
-                        'url("' + SRC_IMG + detailItem.img3[slideNumber] + '")';
-
-                    var navSliderAll = document.querySelectorAll('[' + DATA_ATTR_SLIDE + ']');
-                    Array.prototype.forEach.call(navSliderAll, function (item) {
-                        var itemNumber = +item.getAttribute(DATA_ATTR_SLIDE);
-
-                        if (itemNumber !== slideNumber) {
-                            item.classList.remove('active');
-                        } else {
-                            item.classList.add('active');
-                        }
-                    });
-
-                    return;
-                } else {
-                    if (target.tagName !== 'BODY') {
-                        target = target.parentNode;
-                    } else {
-                        return;
-                    }
-                }
-            }
-        }
     };
 
     return {
@@ -264,3 +177,6 @@ var app = (function () {
  * Dom ready
  * */
 document.addEventListener('DOMContentLoaded', app.init);
+/**
+ * Todo Active nav
+ * */
